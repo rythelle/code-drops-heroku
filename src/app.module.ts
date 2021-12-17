143e3f9a204d796a945bfb07f0b74835451a98e0
Rythelle C. Pereira <rythelle20@hotmail.com>
2021-12-17 13:22:20 -0300

    app.module.ts att

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://root:root@cluster1.mcx0b.mongodb.net/test',
-    ),
-    UsersModule,
-  ],
+  imports: [MongooseModule.forRoot(''), UsersModule],
   controllers: [],
   providers: [],
 })
